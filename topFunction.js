4512dd648a4e0c0e33dd19d8b4ce4aef64266473
Carlos <60042454+carlos-sergio@users.noreply.github.com>
2023-02-16 10:10:51 -0300

    update

--- topFunction.js
+++ topFunction.js
@@ -3,4 +3,24 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
-  
+
+const link = document.querySelector('#link');
+const loader = document.querySelector('#loader2');
+
+link.addEventListener('click', function(event) {
+   event.preventDefault();
+   loader.style.display = 'block';
+  $('#link').click(function(event) {
+    event.preventDefault();
+    $('#loader').show();
+ 
+    $.ajax({
+       type: 'GET',
+       url: 'https://jsonplaceholder.typicode.com/posts',
+       success: function(data) {
+          $('#loader').hide();
+          console.log(data);
+       }
+    });
+ });
+});
